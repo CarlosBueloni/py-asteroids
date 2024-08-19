1d917252923ae7d1f4e24307f6cc8a25e6ae7deb
CarlosBueloni <carlosbueloni@gmail.com>
2024-08-19 16:38:55 -0300

    limit fps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -10,6 +12,8 @@ def main():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
